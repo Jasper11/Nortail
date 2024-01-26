db1a4b6aa9f6ad6576f84c5c085de9023faeaaa2
ifedun <ifedun@mobileiron.com>
2024-01-26 13:39:01 +0200

    removed not needed part

--- src/main/java/pageObjects/LoginScreen.java
+++ src/main/java/pageObjects/LoginScreen.java
@@ -1,19 +1,12 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.PageFactory;
-import utils.HelperClass;
 
 public class LoginScreen extends BaseScreen{
 
     By userNameField = By.xpath("//android.widget.EditText[@content-desc='username']");
     By passwordField = By.xpath("//android.widget.EditText[@content-desc='password']");
     By loginButton = By.xpath("//android.widget.Button");
-
-    public LoginScreen() {
-
-        PageFactory.initElements(HelperClass.getDriver(), this);
-    }
 
     public boolean isLoginScreenDisplayed() {
         return waitForElementVisibility(loginButton).isDisplayed();
