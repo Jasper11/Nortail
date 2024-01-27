73d7145d5cd080b366f3d41538b5e19e4c5227a0
ifedun <ifedun@mobileiron.com>
2024-01-27 16:26:50 +0200

    fixed run approach

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -2,27 +2,16 @@ package steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import utils.HelperClass;
 
 public class Hooks {
-
     @Before
-    public static void setUp() {
-        HelperClass.setUpDriver();
+    public static void launchApp() {
+        HelperClass.getDriver().activateApp("com.vodqareactnative");
     }
 
     @After
-    public static void tearDown(Scenario scenario) {
-
-        //validate if scenario has failed
-        if (scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
-        }
-
-        HelperClass.tearDown();
+    public static void closeApp() {
+        HelperClass.getDriver().terminateApp("com.vodqareactnative");
     }
 }
--- src/test/java/testsRunner/CucumberRunnerTests.java
+++ src/test/java/testsRunner/CucumberRunnerTests.java
@@ -1,10 +1,33 @@
 package testsRunner;
 
+
+import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import utils.HelperClass;
 
 @CucumberOptions(tags = "", features = "src/test/resources/features", glue = "steps",
     plugin = {})
 
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+    @BeforeTest(alwaysRun = true)
+    public static void setUp() {
+        HelperClass.setUpDriver();
+    }
+
+    @AfterTest(alwaysRun = true)
+    public static void tearDown(Scenario scenario) {
+
+        //validate if scenario has failed
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+        HelperClass.tearDown();
+    }
 }
